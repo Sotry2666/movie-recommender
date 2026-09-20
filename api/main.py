004eaa6b3531210bf1d7a3179c200ca2044b0832
Sotry2666 <juaneduardosalazar103@gmail.com>
2026-09-19 20:54:01 -0600

    Fix trailing slash in CORS origin

--- api/main.py
+++ api/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://movie-recommender-phi-seven.vercel.app/"],  
+    allow_origins=["https://movie-recommender-phi-seven.vercel.app"],  
     allow_methods=["*"],
     allow_headers=["*"],
 )
